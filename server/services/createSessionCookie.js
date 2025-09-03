2d37cb670c98c337c11f83be8783f67fe08ac411
DUYBEGINER <phucduygg8485@gmail.com>
2025-09-03 23:32:13 +0700

    update createSessionCookie/ return sessionCookies instead res

--- server/services/createSessionCookie.js
+++ server/services/createSessionCookie.js
@@ -10,10 +10,5 @@ export async function createAndSetSessionCookie(res, idToken) {
   const expiresIn = parseInt(process.env.SESSION_EXPIRES_MS, 10) || 24 * 60 * 60 * 1000; // 1 ngày
   const sessionCookie = await getAuth().createSessionCookie(idToken, { expiresIn });
 
-  res.cookie("session", sessionCookie, {
-    maxAge: expiresIn,
-    httpOnly: true,
-    sameSite: "strict",
-    path: "/",
-  });
+  return {sessionCookie, expiresIn}
 }
