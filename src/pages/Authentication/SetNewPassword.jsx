bae272eedde28b2a8c4e6920f82b244a35087801
DUYBEGINER <phucduygg8485@gmail.com>
2025-09-20 00:41:20 +0700

    add input field in SetNewPassword Page

--- src/pages/Authentication/SetNewPassword.jsx
+++ src/pages/Authentication/SetNewPassword.jsx
@@ -17,6 +17,13 @@ function SetNewPassword() {
     return (
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <h2 className="text-2xl text-center font-bold mb-4">Đặt lại mật khẩu</h2>
+            <form>
+                <label htmlFor="password" className="block text-sm font-medium text-gray-700">Nhập mật khẩu mới</label>
+                <input name="password" type="password" placeholder="Nhập mật khẩu mới" className="border w-full border-gray-300 outline-gray-300 rounded-md p-2" value={email} onChange={handleChange} />
+                <label htmlFor="confirm-password" className="block text-sm font-medium text-gray-700 mt-4">Mật khẩu mới</label>
+                <input name="confirm-password" type="password" placeholder="Nhập lại mật khẩu" className="border w-full border-gray-300 outline-gray-300 rounded-md p-2" />
+                <button type="submit" className="mt-4 w-full bg-blue-500 text-white rounded-md p-2" onClick={handleSubmit}>Xác nhận</button>
+            </form>
         </div>
     );
 }
